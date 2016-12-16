49807b7d5b3c781e3d8d0940565d13ca4a6be158
destino92 <destinodello@gmail.com>
2016-12-16 20:54:42 +0300

    feat: set our pokemon state using data fetched from the pokeapi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'whatwg-fetch';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      pokemon: []
+    };
+
+    this.loadPokemon = this.loadPokemon.bind(this);
+  }
+
+  loadPokemon(url) {
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        console.log(json);
+        this.setState({
+          pokemon: json.results
+        });
+        console.log(this.state);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  componentWillMount(){
+    this.loadPokemon(`${this.props.baseUrl}pokemon/`);
+  }
+
   render() {
     return (
       <div className="App">
