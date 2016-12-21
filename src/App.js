cc02ed00629f87b4296f6f799584178def00ef2d
destino92 <destinodello@gmail.com>
2016-12-21 11:24:48 +0300

    feat: added a model from react-bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'whatwg-fetch';
 import PokemonIndexList from './components/PokemonIndexList';
+import PokemonModal from './components/PokemonModal'
 
 class App extends Component {
 
@@ -16,12 +17,15 @@ class App extends Component {
       offset: 0,
       totalPages: 0,
       count: 0,
-      loaded: false
+      loaded: false,
+      showModal: false
     };
 
     this.loadPokemon = this.loadPokemon.bind(this);
     this.handlePaginationSelect = this.handlePaginationSelect.bind(this);
     this.handleLimitChange = this.handleLimitChange.bind(this);
+    this.handleModalOpen = this.handleModalOpen.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
   }
 
   loadPokemon(url) {
@@ -68,12 +72,24 @@ class App extends Component {
     })
   }
 
+  handleModalOpen(){
+    this.setState({
+      showModal: true
+    });
+  }
+
+  handleModalClose(){
+    this.setState({
+      showModal: false
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Pokemon Dashboard</h2>
+          <h2>Pokemon Dashbo</h2>
         </div>
 
         {this.state.loaded ? null : "Loading..."}
@@ -88,6 +104,10 @@ class App extends Component {
           activePage={this.state.activePage}
           onSelect={this.handlePaginationSelect}
           totalPages={this.state.totalPages} />
+
+          <PokemonModal openModal={this.handleModalOpen}
+          closeModal={this.handleModalClose}
+          showModal={this.state.showModal} />
       </div>
     );
   }
